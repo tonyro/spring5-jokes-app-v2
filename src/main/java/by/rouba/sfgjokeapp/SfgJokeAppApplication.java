da15602fabc7995923e1e919b4f80fa63015af42
tonyro <%%rsmbg9Xh9N29g9hUTUjFQ3mVypBf!KEkYdH#VK!ch#4*7>
2021-10-08 17:58:58 -0400

    Initial commit; added spring boot dependencies

---
+++ src/main/java/by/rouba/sfgjokeapp/SfgJokeAppApplication.java
@@ -0,0 +1,13 @@
+package by.rouba.sfgjokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgJokeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgJokeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/rouba/sfgjokeapp/SfgJokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.rouba.sfgjokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJokeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
